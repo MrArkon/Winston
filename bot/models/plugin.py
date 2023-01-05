8036b455cb9fc82c841d6d0ac6387624fa2463be
MrArkon <mrarkon@outlook.com>
2023-01-05 10:12:51 +0530

    Add plugin model

--- bot/models/plugin.py
+++ bot/models/plugin.py
@@ -15,5 +15,26 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-from .plugin import Plugin
-from .tree import CommandTree
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from discord.ext import commands
+
+if TYPE_CHECKING:
+    from bot import Winston
+
+
+class Plugin(commands.Cog):
+    """
+    Represents a base cog class. Implements the default :meth:`__init__` which contains
+    the bot attribute.
+
+    Parameters
+    ----------
+    bot: :class:`Winston`
+        The main bot instance
+    """
+
+    def __init__(self, bot: Winston) -> None:
+        self.bot = bot
