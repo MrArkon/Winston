0c0eaeae97f50ae99154ce404426b79824d5d47d
MrArkon <mrarkon@outlook.com>
2023-01-08 12:02:05 +0530

    Add server information command

--- bot/plugins/misc.py
+++ bot/plugins/misc.py
@@ -20,10 +20,10 @@ import time
 import discord
 from discord import app_commands as app
 
-from bot import Winston, config, models
+from bot import Plugin, Plural, Winston, config
 
 
-class Miscellaneous(models.Plugin):
+class Miscellaneous(Plugin):
     @app.command()
     async def ping(self, interaction: discord.Interaction) -> None:
         """Measures the latency and response time of the bot."""
@@ -57,7 +57,7 @@ class Miscellaneous(models.Plugin):
         embed.set_author(name=str(user), icon_url=user.display_avatar.url)
 
         embed.description = f"{user.mention} " + " ".join(
-            str(config.EMOJIS.get(name, "")) for name, value in user.public_flags if value
+            str(config.BADGES.get(name, "")) for name, value in user.public_flags if value
         )
 
         embed.add_field(
@@ -101,6 +101,84 @@ class Miscellaneous(models.Plugin):
             text=f"ID: {user.id}"
             + (" | This user is not a member of this server." if not isinstance(user, discord.Member) else "")
         )
+
+        await interaction.response.send_message(embed=embed, ephemeral=ephemeral)
+
+    @info.command()
+    @app.describe(ephemeral="Setting this to True makes it so that only you can see it. Default is False.")
+    async def server(self, interaction: discord.Interaction, ephemeral: bool = False) -> None:
+        """Obtain information about this server"""
+        embed = discord.Embed(color=discord.Color.blurple())
+        embed.set_author(name=str(interaction.guild), icon_url=getattr(interaction.guild.icon, "url", None))
+
+        embed.add_field(name="Owner", value=interaction.guild.owner.mention)
+
+        embed.add_field(
+            name="Created At",
+            value="\n".join(discord.utils.format_dt(interaction.guild.created_at, style=f) for f in ["D", "R"]),  # type: ignore
+        )
+
+        assert interaction.guild.member_count is not None
+        bots = sum(member.bot for member in interaction.guild.members)
+        embed.add_field(
+            name="Members",
+            value=f"{interaction.guild.member_count} Total\n"
+            f"{Plural(interaction.guild.member_count - bots):Human} | "
+            f"{Plural(bots):Bot}\n",
+        )
+
+        text_channels = [0, 0]
+        voice_channels = [0, 0]
+
+        for channel in interaction.guild.channels:
+            allowed, denied = channel.overwrites_for(interaction.guild.default_role).pair()
+            permissions = discord.Permissions(
+                (interaction.guild.default_role.permissions.value & ~denied.value) | allowed.value
+            )
+
+            if isinstance(channel, discord.TextChannel):
+                text_channels[0] += 1
+                text_channels[1] += 1 if not permissions.read_messages else 0
+            elif isinstance(channel, (discord.VoiceChannel, discord.StageChannel)):
+                voice_channels[0] += 1
+                voice_channels[1] += 1 if not permissions.connect else 0
+
+        embed.add_field(
+            name="Channels",
+            value=f"{config.TEXT_CHANNEL} {text_channels[0]}"
+            + ("\n" if not text_channels[1] else f" ({text_channels[1]} Locked)\n")
+            + f"{config.VOICE_CHANNEL} {voice_channels[0]}"
+            + ("" if not voice_channels[1] else f" ({voice_channels[1]} Locked)\n"),
+        )
+
+        embed.add_field(
+            name="Moderation",
+            value=f"2FA: {'Enabled' if interaction.guild.mfa_level.value else 'Disabled'}\n"
+            f"Explicit Media Filter: {['Disabled', 'Medium', 'High'][interaction.guild.explicit_content_filter.value]}\n"
+            f"Verification: {str(interaction.guild.verification_level).title()}",
+        )
+
+        last_boost = max(interaction.guild.members, key=lambda member: member.premium_since or interaction.guild.created_at)
+        embed.add_field(
+            name="Boosts",
+            value=f"Level {interaction.guild.premium_tier} ({interaction.guild.premium_subscription_count} Boosts)"
+            + (
+                f"Last Boost: {last_boost} ({discord.utils.format_dt(last_boost.premium_since, style='R')})"
+                if last_boost.premium_since is not None
+                else ""
+            ),
+        )
+
+        embed.add_field(
+            name="Server Limits",
+            value=f"Emojis: {len(interaction.guild.emojis)}/{interaction.guild.emoji_limit}\n"
+            f"Bitrate: {int(interaction.guild.bitrate_limit / 1000)} Kbps\n"
+            f"File Size Limit: {int(interaction.guild.filesize_limit / 1048576)} MB\n"
+            f"Maximum Members: {interaction.guild.max_members:,}",
+            inline=False,
+        )
+
+        embed.set_footer(text=f"ID: {interaction.guild.id}")
 
         await interaction.response.send_message(embed=embed, ephemeral=ephemeral)
 
